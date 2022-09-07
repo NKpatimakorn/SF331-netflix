18cdeb9aa834b4fa0066255272edbac192f63306
Nickname <patimakorn.ran@dome.tu.ac.th>
2022-09-07 11:56:17 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [movie, setMovie] = useState({});
+    useEffect(()=>{
+        axios.get(`http://netflix.sf331.site/movies.json`).then((response) => {
+            setMovie(response.data);
+        });
+    },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+        <h1>TU-Flix</h1>
+        {movie.map(m =>
+            <div>
+                <h3>{m.Title}</h3>
+                <p>{m.Plot}</p>
+            </div>
+        )}
     </div>
   );
 }
